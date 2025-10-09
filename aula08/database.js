e7bd9d9476dbd461994c55041ed296e97e4fae53
Arthur Amorim <arthur.silva3@iesb.edu.br>
2025-10-09 09:16:59 -0300

    Adicionando aula08

---
+++ aula08/database.js
@@ -0,0 +1,18 @@
+const {MongoClient} = require('mongodb');
+
+const url = "mongodb+srv://arthursilva3_db_user:database@cluster0.ya7jadv.mongodb.net/";
+
+const client = new MongoClient(url);
+
+async function conectar() {
+    try{
+        await client.connect();
+        console.log("Conectado");
+        return client.db("agenda");
+    }catch(e) {
+        console.log("Erro ao conectar no MongoDB",
+            e.message);
+    }
+}
+
+module.exports = conectar;
