c321279fc7958cf4cb540931007bd9d57e9cbd59
Arthur Amorim <arthur.silva3@iesb.edu.br>
2025-10-10 10:45:52 -0300

    Adicionando aula09

---
+++ aula/aula09/database.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/`;
+
+const client = new MongoClient(url);
+let db = null;
+
+async function conectar() {
+  try {
+    if (db == null) {
+      await client.connect();
+      db = client.db("agenda");
+    }
+    return db;
+  } catch (e) {
+    console.log("erro ao conectar ao mongodb", e.message);
+  }
+}
+
+module.exports = conectar;
